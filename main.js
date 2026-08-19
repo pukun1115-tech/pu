2d079c8010b9bdfac4fedf8c3648c8ee72c73a69
pukun1115-tech <pukun1115@gmail.com>
2026-08-19 11:42:38 +0900

    5*5にしてみた

--- main.js
+++ main.js
@@ -31,8 +31,8 @@ const camera = {
     isZBuffer: true
 };
 
-for (let i = 0; i < 3; i++) {
-    for (let j = 0; j < 3; j++) {
+for (let i = 0; i < 5; i++) {
+    for (let j = 0; j < 5; j++) {
         chunks.push(new chunk(i, j));
     }
 }
