d8c2c0ac27352616dec59f7380c61d006519063e
pukun1115-tech <pukun1115@gmail.com>
2026-08-19 11:48:37 +0900

    変数のスコープ

--- main.js
+++ main.js
@@ -5,6 +5,10 @@ const keys = {};//キーの状態
 document.addEventListener("keydown", e => keys[e.key] = true);//キーが押された時
 document.addEventListener("keyup", e => keys[e.key] = false);//キーが押されてない時
 
+//
+let depthBuffer = null;
+let colorBuffer = null;
+
 //zBufferをclear
 function clearBuffers() {
     const far = 1e9;//とても大きい
@@ -70,8 +74,8 @@ function mainLoop() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     resize();
 
-    const depthBuffer = new Float32Array(canvas.width * canvas.height);//一次元
-    const colorBuffer = new Uint32Array(canvas.width * canvas.height);
+    depthBuffer = new Float32Array(canvas.width * canvas.height);//一次元
+    colorBuffer = new Uint32Array(canvas.width * canvas.height);
 
     for (const c of chunks) {
         c.generateTriangles();
